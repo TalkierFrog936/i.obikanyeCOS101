877994ebde947bd60f2e19292ca005fba4fd2471
TalkierFrog936 <iyinobikanye@gmail.com>
2023-11-02 16:38:53 +0100

    (projects)

---
+++ week3/practice1/target/debug/deps/practice1.d
@@ -0,0 +1,5 @@
+C:\Users\iyino\Documents\i.obikanyeCOS101\week3\practice1\target\debug\deps\practice1.exe: src\main.rs
+
+C:\Users\iyino\Documents\i.obikanyeCOS101\week3\practice1\target\debug\deps\practice1.d: src\main.rs
+
+src\main.rs:
---
+++ week3/practice1/target/debug/practice1.d
@@ -0,0 +1,1 @@
+C:\Users\iyino\Documents\i.obikanyeCOS101\week3\practice1\target\debug\practice1.exe: C:\Users\iyino\Documents\i.obikanyeCOS101\week3\practice1\src\main.rs
